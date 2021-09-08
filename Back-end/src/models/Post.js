9583b0ed10d6b6b7d3e94bf1950cff3a0f30172e
Kevin Le Van Phung <levanphungk@gmail.com>
2021-09-08 16:57:19 +0200

    post model implemented

---
+++ Back-end/src/models/Post.js
@@ -0,0 +1,21 @@
+const { Sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) =>
+    sequelize.define('Post', {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        ownerId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        date: {
+            type: 'TIMESTAMP',
+            allowNull: false
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    })
