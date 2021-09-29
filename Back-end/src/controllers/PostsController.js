c0dbdb97cacc59accdbe909f4eb5dc88c21970c7
Kevin Le Van Phung <levanphungk@gmail.com>
2021-09-29 12:40:18 +0200

    removed useless commented lines

--- Back-end/src/controllers/PostsController.js
+++ Back-end/src/controllers/PostsController.js
@@ -89,31 +89,7 @@ module.exports  = {
             })
         }
     },
-
-    // async editPost (req, res) {
-    //     try {
-    //         const post = await Post.update(req.body, {
-    //             where: {
-    //                 id: req.params.id
-    //             }
-    //         })
-    //         if (!post) {
-    //             return res.status(404).send({
-    //                 error: 'Couldn\'t update the Post'
-    //             })
-    //         }
-    //         const updated = await Post.findOne({
-    //             where: {
-    //                 id: req.params.id
-    //             }
-    //         })
-    //         res.status(200).send(updated)
-    //     } catch {
-    //         res.status(500).send({
-    //             error: 'An error occured trying to update the Post'
-    //         })
-    //     }
-    // },
+    
     async editPost (req, res) {
         const postObject = req.file ? {
             ...req.body,
